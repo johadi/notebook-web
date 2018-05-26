34b985200184c6a1b91740c55a4374d82f805af2
johadi <jimoh.hadi@gmail.com>
2018-05-26 22:28:31 +0100

    chore(setUp): add webpack and eslint config files

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
+// variable that holds package.json script command for running the webpack config file
+// in production mode or in development mode.
+const script = {
+  dev: 'build:dev',
+  // prod: 'build'
+  prod: 'heroku-postbuild',
+  all: 'build:all'
+};
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'dist'),
+};
+
+const common = {
+  context: PATHS.app,
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
+  entry: {
+    app: './index.jsx'
+  },
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new webpack.DefinePlugin({
+      // API_HOST is used for production
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        API_HOST: JSON.stringify(process.env.API_HOST || `http://localhost:${process.env.PORT || 5000}`)
+      }
+    })
+  ],
+  module: {
+    rules: [
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: PATHS.app
+      },
+      {
+        test: /\.(scss|css)?$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          // resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+        })
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader?limit=250000'
+      }
+    ]
+  },
+};
+// Default configuration
+if (TARGET === script.dev || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT,
+      proxy: {
+        '/api/**': {
+          target: 'http://localhost:4000',
+          secure: false
+        }
+      }
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      // new DashboardPlugin()
+    ]
+  });
+}
+if (TARGET === script.prod || TARGET === script.all) {
+  module.exports = merge(common, {});
+}
+
